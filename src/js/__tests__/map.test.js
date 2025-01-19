97847f6c3ae7547bb4d9c401f4f2213f44a76838
ailunaM <ajlunaravsanbekova@gmail.com>
2025-01-19 15:50:15 +0600

    task№2 done with tests

--- src/js/__tests__/map.test.js
+++ src/js/__tests__/map.test.js
@@ -1,1 +1,15 @@
-test("", () => {});
+// test("", () => {});
+import { ErrorRepository } from "../map";
+
+describe("ErrorRepository class tests", () => {
+  let repository;
+  beforeEach(() => {
+    repository = new ErrorRepository();
+  });
+  test("translate method throws a known error", () => {
+    expect(repository.translate(400)).toBe("Bad Request");
+  });
+  test("translate method returns unknown error", () => {
+    expect(repository.translate(123)).toBe("Unknown error");
+  });
+});
--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,14 @@
-
+export class ErrorRepository {
+  constructor() {
+    this.errors = new Map([
+      [404, "Not Found"],
+      [500, "Internal Server Error"],
+      [400, "Bad Request"],
+    ]);
+  }
+  translate(code) {
+    return this.errors.get(code) || "Unknown error";
+  }
+}
+const repository = new ErrorRepository();
+console.log(repository.translate(123));
